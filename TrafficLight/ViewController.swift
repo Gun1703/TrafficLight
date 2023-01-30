2b9b5a829c7d64dc238a066dea5237deb9e8a052
Gun <ggunin1992@gmail.com>
2023-01-31 02:33:13 +0300

    firstScreen

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 50
+        lightButton.layer.cornerRadius = 15
+        
     }
-
-
+    
+    @IBAction func lightButtonDidTapped() {
+        lightButton.setTitle("NEXT", for: .normal); redLightView.alpha = 1
+    }
+    
 }
-
